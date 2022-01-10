15ab1935fc7812fbc1644418ffbb4e7ac97a266b
Pablo <ppmoya08@gmail.com>
2022-01-09 22:28:14 -0300

    added changes

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def root():
+    return "You'r in the root"
+
+@app.route("/hello")
 def hello():
-    return "Hello, World!"
+    return "Hello"
